d60bec3063a3fddd2a8331fc710db41ff36b8091
Cb92711111 <Cejay.baxter27@ycdsbk12.ca>
2026-06-04 12:29:57 -0400

    Automate road lanes to go from top to bottom

--- src/Sketch.java
+++ src/Sketch.java
@@ -2,12 +2,14 @@ import processing.core.PApplet;
 
 /**
  * Template for programs with Processing graphics output.
- * @author Mitchell Jaja & Cejay Baxter
+ * @author Mitchell & Cejay
  */
 public class Sketch extends PApplet {
     int roadColour = color(255);
     int carX = 394; // Center lane
     int carY = 750; // Bottom of screen
+    int roadY = 0;
+    int roadSpeed = 5;
 
     public static void main(String[] args) {
         PApplet.main("Sketch");
@@ -20,8 +22,7 @@ public class Sketch extends PApplet {
 
     @Override
     public void setup() {
-        generateBackground();
-        drawCar();
+        
     }
 
     public void generateBackground(){
@@ -56,28 +57,60 @@ public class Sketch extends PApplet {
     public void drawCar(){
         noStroke();
         
-        // Tires (Black rectangles)
+        // Tires
         fill(0);
-        rect(carX - 25, carY - 20, 8, 15);  // Front Left
-        rect(carX + 17, carY - 20, 8, 15);  // Front Right
+        rect(carX- 25, carY - 20, 8, 15);  // Front Left
+        rect(carX + 17,carY - 20, 8, 15);  // Front Right
         rect(carX - 27, carY + 15, 10, 18); // Rear Left
-        rect(carX + 17, carY + 15, 10, 18); // Rear Right
+        rect(carX + 17,carY + 15, 10, 18); // Rear Right
 
-        // Main Car Body (Orange rectangle)
         fill(255, 100, 0); 
         rect(carX - 20, carY - 20, 40, 55); 
         
-        // Pointy Nose (Triangle)
-        triangle(carX - 20, carY - 20, carX + 20, carY - 20, carX, carY - 40);
+        // Nose
+        triangle(carX-20, carY-20, carX+ 20, carY-20, carX, carY- 40);
 
-        // Windshield (Dark box)
+        // Windshield
         fill(30, 30, 50);
         rect(carX - 12, carY - 10, 24, 15);
     }
 
     @Override
     public void draw() {
+        moveBackground();
+        drawRoad();
+    
+        drawCar();
 
+    }
+
+    private void drawRoad() {
+        background(128,128, 128);
+        fill(roadColour);
+        noStroke();
+
+        for (int y = -40 + roadY; y < height; y += 40) {
+            rect(304, y, 11, 20);
+            rect(485, y, 11, 20); 
+        }
+
+        // Grass
+        fill(0, 180, 0);
+        rect(0, 0, 133, 900);
+        rect(667, 0, 133, 900);
+
+        // Yellow Borders
+        stroke(255, 255, 0);
+        strokeWeight(7);
+        line(133, 0, 133, 900);
+        line(667, 0, 667, 900);
+    }
+
+    private void moveBackground() {
+        roadY += roadSpeed;
+        if (roadY >= 40) {
+        roadY = 0;
+        }
     }
 
     /** Additional helper methods below */
